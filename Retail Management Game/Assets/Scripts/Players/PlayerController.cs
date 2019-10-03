575a2208d7a3800e571139b389d0fb301b890cef
Edisson Flores <flores96@gmail.com>
2019-10-03 03:33:01 -0400

    Studying new Unity Input System
    Build v0.1.0.1
    Unity Version: 2019.1.14f1
    
    Imported Unity Input System package
    Imported Unity Shader Stack package
    
    PlayerController.cs
    + Controllers player character

---
+++ Retail Management Game/Assets/Scripts/Players/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] InputAction movement;
+    [SerializeField] InputActionAsset playerControls;
+
+    private void Awake()
+    {
+        InputActionMap gameplayActionMap = playerControls.FindActionMap("Gameplay");
+
+        movement = gameplayActionMap.FindAction("Movement");
+
+        movement.performed += OnMovementChanged;
+    }
+
+    private void OnDisable()
+    {
+        movement.Disable();
+    }
+
+    private void OnEnable()
+    {
+        movement.Enable();
+    }
+
+    private void OnMovementChanged(InputAction.CallbackContext context)
+    {
+        Vector2 direction = context.ReadValue<Vector2>();
+
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
